94a44bb9ba3c70d234a555520f0a7b72ebf48d5d
Elaidzha1940 <eli080293@gmail.com>
2024-03-27 14:38:29 +0300

    LottieAnimation

---
+++ LottieAnimation/ContentView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: LottieAnimation
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.03.2024
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LottieAnimation/LottieAnimationApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: LottieAnimation
+//  File: LottieAnimationApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.03.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct LottieAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
